5c990521e7d6f2b11ffe15c0d54da8cf21ab0ac4
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-02-11 03:31:26 +0100

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import Cardlist from "@/components/Cardlist";
 import Hero2 from "@/components/heroOptions/Hero2";
-import Testemonials from "@/components/testimonials";
+import Testimonials from "@/components/testimonials";
 
 export default function Home() {
   return (
     <div className="">
       <Hero2 />
       <Cardlist />
-      <Testemonials />
+      <Testimonials />
     </div>
   );
 }
